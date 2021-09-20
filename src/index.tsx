629f450662af8d76feb55946b171eccc9fb6455d
kimelijahsol <kimelijahsol@gmail.com>
2021-09-20 15:22:28 +0900

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style/index.css";
+import NumberCounter from "./lib/number-counter";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="col col-1">
+      <NumberCounter
+        value={0.32}
+        className="fs-16"
+        transition={130}
+        suffix="%"
+      />
+      <NumberCounter
+        value={1923400}
+        id="krw1"
+        transition={130}
+        suffix="₩/KRW"
+      />
+    </div>
+    <div className="col col-2">
+      <NumberCounter
+        value={0.32}
+        className="fs-16"
+        transition={130}
+        suffix="%"
+      />
+      <NumberCounter
+        value={1923400}
+        id="krw2"
+        transition={130}
+        suffix="₩/KRW"
+      />
+    </div>
+    <div className="col col-3">
+      <NumberCounter
+        value={0.32}
+        className="fs-16"
+        transition={130}
+        suffix="%"
+      />
+      <NumberCounter
+        value={1923400}
+        id="krw3"
+        transition={130}
+        suffix="₩/KRW"
+      />
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
